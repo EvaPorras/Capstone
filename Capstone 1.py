a8af004ab03cbc8d34991438563b6248593b5b11
EvaPorras <54546520+EvaPorras@users.noreply.github.com>
2019-08-27 09:13:25 +0200

    Capstone Week 1 Assignment

---
+++ Capstone 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # CAPSTONE 1
+# This notebook will be mainly used for the capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print ("Hello Capstone Project Course!")
+
